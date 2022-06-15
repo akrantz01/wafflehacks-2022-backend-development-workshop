3745639e89a78ad72918de1ce1ec30de7181d34e
Alex Krantz <alex@krantz.dev>
2022-06-15 11:13:29 +0200

    Ensure foreign key constraints work

--- todo_list/database.py
+++ todo_list/database.py
@@ -1,10 +1,23 @@
 from dataclasses import dataclass
 from os import environ
+from sqlite3 import Connection as Sqlite3Connection
 from typing import List as ListT, Optional
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
 
 db = SQLAlchemy()
+
+
+def _set_sqlite_pragma(connection, _record):
+    """
+    Enables foreign key constraints on sqlite database connections. This is required as sqlite does not support
+    foreign key constraints by default, and they must be enabled per-connection.
+    """
+    if isinstance(connection, Sqlite3Connection):
+        cursor = connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
 
 
 def init_db(app):
@@ -12,6 +25,10 @@ def init_db(app):
     Connect SQLAlchemy to the Flask app
     """
     db.init_app(app)
+
+    # Add the event listener for automatically enabling foreign key constraints
+    with app.app_context():
+        event.listen(db.engine, "connect", _set_sqlite_pragma)
 
     # Allow resetting the database
     reset = environ.get("DATABASE_RESET", "").lower()
