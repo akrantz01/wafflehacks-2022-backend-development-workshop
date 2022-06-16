0428f81068bc79bd7ab87dce210d4a6fbc37de6c
Alex Krantz <alex@krantz.dev>
2022-06-16 23:14:00 +0200

    Implement going back instead of going home

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,18 +1,18 @@
-import { AnchorButton, NonIdealState } from '@blueprintjs/core';
+import { Button, NonIdealState } from '@blueprintjs/core';
 import type { NextPage } from 'next';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const NotFound: NextPage = () => (
-  <NonIdealState
-    icon="help"
-    title="Page not found"
-    description="The page you are looking for couldn't be found. Please check it is correct and try again."
-    action={
-      <Link href="/" passHref>
-        <AnchorButton>Return Home</AnchorButton>
-      </Link>
-    }
-  />
-);
+const NotFound: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <NonIdealState
+      icon="help"
+      title="Page not found"
+      description="The page you are looking for couldn't be found. Please check it is correct and try again."
+      action={<Button onClick={router.back}>Go back</Button>}
+    />
+  );
+};
 
 export default NotFound;
